3379345b4bda6ece1b10eea2c7a1f9b518fd141e
Peter Schleifer <peter.schleifer@usa.net>
2015-06-05 11:28:57 -0400

    Initial draft of first plot

---
+++ plot1.R
@@ -0,0 +1,50 @@
+sourceUrl = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+zipFile = "exdata_data_household_power_consumption.zip"
+dataFile = "./household_power_consumption.txt"
+
+# Make sure we have the needed packages loaded
+if ( !(require(dplyr) && require(lubridate)) ) {
+    stop ("You need to install the dplyr & libridate packages to run this script")
+}
+
+# Download and unzip file if it does not exist
+if ( !file.exists(dataFile) ) {
+    download.file(sourceUrl,zipFile)
+    unzip(zipFile)
+}
+
+# File heading delimited by periods, data delimited by semi-colons, ?'s represent NA
+electricPowerConsumption = tbl_df(read.table(dataFile, skip=1, sep=";", 
+                                      col.names=c("Date",
+                                                  "Time",
+                                                  "Global_active_power",
+                                                  "Global_reactive_power",
+                                                  "Voltage",
+                                                  "Global_intensity",
+                                                  "Sub_metering_1",
+                                                  "Sub_metering_2",
+                                                  "Sub_metering_3"),
+                                      na.strings="?"))
+
+# Convert date & time and filter to rows of interest
+electricPowerConsumption <-
+    electricPowerConsumption %>%
+    mutate(dateTime = dmy_hms(paste(Date,Time))) %>%
+    filter(as.Date(dateTime) == as.Date("2007-02-01") |
+               as.Date(dateTime) == as.Date("2007-02-02") ) %>%
+    # Don't need original date and time columns
+    select( -(Date:Time) )
+
+# Open the graphics device
+png( filename="plot1.png", width=480, height=480 )
+
+# Generate the plot
+hist(electricPowerConsumption$Global_active_power
+     ,xlab="Global Active Power (kilowatts)", 
+     col="red", 
+     main="Global Active Power",
+     # Reduce the font on the axis labels so it shows labels on every tick per the requirement
+     cex.axis=0.7)
+
+# Close file
+dev.off()
